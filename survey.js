a61046043fe2c8a82e2a2fed289c873bbad2c9e9
AD202310 <a.g.dychenko@gmail.com>
2023-11-21 21:22:19 -0500

    remove unnecessary items and linted

--- survey.js
+++ survey.js
@@ -6,57 +6,34 @@ const rl = readline.createInterface({
 });
 
 const question = (question) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     rl.question(question, (answer) => {
-      resolve(answer)
-    })
-  })
-}
+      resolve(answer);
+    });
+  });
+};
 
-const main = async () => {
+const main = async() => {
   const q1 = await question(
     'What\'s your name? Nicknames are also acceptable :) ');
-  const q2= await question(
+  const q2 = await question(
     'What\'s an activity you like doing? ');
-  const q3= await question(
-    'What do you listen to while doing that? '); 
-  const q4= await question(
+  const q3 = await question(
+    'What do you listen to while doing that? ');
+  const q4 = await question(
     'Which meal is your favourite (eg: dinner, brunch, etc.) ');
-  const q5= await question(
+  const q5 = await question(
     'What\'s your favourite thing to eat for that meal? ');
-  const q6= await question(
-    'Which sport is your absolute favourite? ');   
-  const q7= await question(
-    'What is your superpower? In a few words, tell us what you are amazing at! '); 
+  const q6 = await question(
+    'Which sport is your absolute favourite? ');
+  const q7 = await question(
+    'What is your superpower? In a few words, tell us what you are amazing at! ');
 
   console.log(`\n 
   
   ${q1} loves listening to ${q3} while ${q2}, devouring ${q4} for ${q5}, preferes ${q6} over any other sport, and is amazing at ${q7}.
   `);
-  rl.close()
-}
+  rl.close();
+};
 
-main()
-
-
-// rl.question(
-//   'What\'s your name? Nicknames are also acceptable :) ',
-//   (answer) => {
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-//   rl.close();
-// });
-
-// rl.question(
-//   'What\'s your name? Nicknames are also acceptable :) ',
-//   (answer) => {
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//   rl.close();
-// });
-
-
-
-// rl.question('What do you think of Node.js? ', (answer) => {
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
-
-//   rl.close();
+main();
